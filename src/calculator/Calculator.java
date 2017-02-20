ff7b284473e15ca6ae139baefcc7c121c77e6b02
Stephannie Tan <akosimeilingtan@gmail.com>
2017-02-20 19:47:54 +0800

    Initial Commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculator;
+
+
+/**
+ *
+ * @author IstipanyaGrasya
+ */
+
+import java.util.Scanner;
+//import javax.swing.JOptionPane;
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num1;
+        int num2;
+        String operation;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Fisr Number: ");
+        num1 = input.nextInt();
+
+        System.out.println("Second Number: ");
+        num2 = input.nextInt();
+
+        Scanner op = new Scanner(System.in);
+
+      
+        operation = op.next();
+
+        if (operation == "+");
+        {
+            System.out.println("Additon = " + (num1 + num2));
+        }
+        if  (operation == "-");
+        {
+            System.out.println("Subtraction = " + (num1 - num2));
+        }
+
+        if (operation == "/");
+        {
+            System.out.println("Division = " + (num1 / num2));
+        }
+        if (operation == "*")
+        {
+            System.out.println("Multiplication = 6" + (num1 * num2));
+        }
+
+
+    }
+}
+    
+
